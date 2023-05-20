f36f2e17ec710214db8678ab354fb75f1ad7f126
siida36 <siida0118@gmail.com>
2023-05-20 11:04:26 +0900

    Add settings to show Neotree on startup

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
-print("This is my config.")
-return {}
+return {
+  vim.api.nvim_create_autocmd("VimEnter", {
+    command = "set nornu nonu | Neotree toggle",
+  }),
+  vim.api.nvim_create_autocmd("BufEnter", {
+    command = "set rnu nu",
+  })
+}
